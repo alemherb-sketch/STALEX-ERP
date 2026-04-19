de32105a926a60cca0e85614d553ee76ca40b11e
Alem <alemherb@gmail.com>
2026-04-19 00:56:47 -0500

    Fix: Forzado de produccion (DEBUG=False) y creacion automatica de superusuario admin

--- build.sh
+++ build.sh
@@ -4,5 +4,8 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --clear
 python manage.py migrate
+
+# Create superuser if it doesn't exist
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin1234')"
